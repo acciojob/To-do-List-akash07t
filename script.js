8cf4d16ed9d8c11642b12fa4be0059e6c1c9fc4d
Akash Tiwari <124699923+akash07t@users.noreply.github.com>
2023-04-13 17:53:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-//your code here
+function myfun() {
+	
+	var newTodo = document.getElementById("newTodoInput").value;
+	 var listItem = document.createElement("li");
+	if(newTodo == ""){
+		alert("type something");
+	}
+	else{
+	listItem.textContent = newTodo;
+	var todoList = document.getElementById("todoList");
+	  todoList.append(listItem);
+}
+}
 
